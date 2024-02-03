b106ce7a53042825d8fcd835f5105010d05df91b
Brad Cole <artwired@users.noreply.github.com>
2024-02-02 22:50:28 -0800

    updated score text styles

--- quizzical/src/CalcScore.jsx
+++ quizzical/src/CalcScore.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function CalcScore({ score, onPlayAgain }) {
   return (
     <div className="score-container">
-      <p>{`You scored ${score}/5 correct answers`}</p>
+      <p className="answer-txt">{`You scored ${score}/5 correct answers`}</p>
       <button className="large-btn" onClick={onPlayAgain}>
         Play again
       </button>
